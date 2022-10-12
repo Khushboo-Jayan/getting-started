72b3dacbb0cda9f66a406d84d572d2ff63d6587e
Khushboo Jayan <khushboojayan@gmail.com>
2022-10-12 16:18:30 +0100

    lab2

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">Error!404 nothing on the website</p>
+                <p className="text-center">You have no todo items yet! Add one above!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
